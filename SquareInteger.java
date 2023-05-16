12476c220168c5e3cd56b04f55ca6e5a58e77709
CamB102 <120146979+CamB102@users.noreply.github.com>
2023-05-16 14:03:12 +1000

    Add files via upload

---
+++ SquareInteger.java
@@ -0,0 +1,29 @@
+package algorithmday1;
+
+public class SquareIntegerOn {
+
+	public static void main(String[] args) {
+		int a = 28;
+		int b = 56;
+
+		int count = numberOfSquareInteger(a, b);
+		System.out.println("Number of square integers between " + a + " and " + b + ": " + count);
+
+	}
+
+	public static int numberOfSquareInteger(int a, int b) {
+		int count = 0;
+
+		int squareRoot = (int) Math.sqrt(a);
+
+		while (squareRoot * squareRoot <= b) {
+			if (squareRoot * squareRoot >= a) {
+				count++;
+				System.out.println(squareRoot);
+			}
+			squareRoot++;
+		}
+
+		return count;
+	}
+}
